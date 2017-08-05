624073b6f62f51c6f3965ea25ca1f0315a9bf971
Seaton Jiang <seaton@vtrois.com>
2017-08-05 21:37:30 +0800

    Add some smiley pic

--- smiley.php
+++ smiley.php
@@ -41,25 +41,29 @@
 /* ]]> */
 </script>
 <?php $smilies = '
-<a href="javascript:grin(\':razz:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_razz.png" alt="" class="size-smiley"/></a>
-<a href="javascript:grin(\':sad:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_sad.png" alt="" class="size-smiley"/></a>
-<a href="javascript:grin(\':evil:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_evil.png" alt="" class="size-smiley"/></a>
-<a href="javascript:grin(\':exclaim:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_exclaim.png" alt="" class="size-smiley"/></a>
-<a href="javascript:grin(\':smile:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_smile.png" alt="" class="size-smiley"/></a>
-<a href="javascript:grin(\':redface:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_redface.png" alt="" class="size-smiley"/></a>
-<a href="javascript:grin(\':biggrin:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_biggrin.png" alt="" class="size-smiley"/></a>
-<a href="javascript:grin(\':surprised:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_surprised.png" alt="" class="size-smiley"/></a>
-<a href="javascript:grin(\':eek:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_eek.png" alt="" class="size-smiley"/></a>
-<a href="javascript:grin(\':confused:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_confused.png" alt="" class="size-smiley"/></a>
-<a href="javascript:grin(\':idea:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_idea.png" alt="" class="size-smiley"/></a>
-<a href="javascript:grin(\':lol:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_lol.png" alt="" class="size-smiley"/></a>
-<a href="javascript:grin(\':mad:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_mad.png" alt="" class="size-smiley"/></a>
-<a href="javascript:grin(\':twisted:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_twisted.png" alt="" class="size-smiley"/></a>
-<a href="javascript:grin(\':rolleyes:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_rolleyes.png" alt="" class="size-smiley"/></a>
-<a href="javascript:grin(\':wink:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_wink.png" alt="" class="size-smiley"/></a>
-<a href="javascript:grin(\':cool:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_cool.png" alt="" class="size-smiley"/></a>
-<a href="javascript:grin(\':arrow:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_arrow.png" alt="" class="size-smiley"/></a>
-<a href="javascript:grin(\':neutral:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_neutral.png" alt="" class="size-smiley"/></a>
-<a href="javascript:grin(\':cry:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_cry.png" alt="" class="size-smiley"/></a>
-<a href="javascript:grin(\':mrgreen:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/icon_mrgreen.png" alt="" class="size-smiley"/></a>'
-?>
+<a href="javascript:grin(\':razz:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/razz.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':evil:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/evil.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':exclaim:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/exclaim.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':smile:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/smile.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':redface:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/redface.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':biggrin:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/biggrin.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':surprised:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/surprised.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':eek:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/eek.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':confused:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/confused.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':idea:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/idea.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':lol:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/lol.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':mad:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/mad.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':twisted:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/twisted.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':rolleyes:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/rolleyes.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':wink:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/wink.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':cool:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/cool.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':arrow:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/arrow.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':neutral:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/neutral.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':cry:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/cry.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':mrgreen:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/mrgreen.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':drooling:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/drooling.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':cowboy:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/cowboy.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':persevering:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/persevering.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':symbols:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/symbols.png" alt="" class="size-smiley"/></a>
+<a href="javascript:grin(\':shit:\')"><img src="'.get_bloginfo("template_url").'/images/smilies/shit.png" alt="" class="size-smiley"/></a>
+'?>
