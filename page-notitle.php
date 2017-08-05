5d74ea710a8041c82a27f68f31c2e69cd44b3697
Seaton Jiang <seaton@vtrois.com>
2017-08-05 21:15:24 +0800

    Integration share function

--- page-notitle.php
+++ page-notitle.php
@@ -29,66 +29,7 @@ get_header('banner'); ?>
 					   			<?php endif; ?>
 								<?php if ( kratos_option( 'page_share' )==1 ) : ?>
 								<a href="javascript:;"  class="Share" ><i class="fa fa-share-alt"></i> 分享</a>
-								<div class="share-wrap" style="display: none;">
-								<div class="share-group">
-									<a href="javascript:;" class="share-plain twitter" onclick="share('qq');" rel="nofollow">
-										<div class="icon-wrap">
-											<i class="fa fa-qq"></i>
-										</div>
-									</a>
-									<a href="javascript:;" class="share-plain weibo" onclick="share('weibo');" rel="nofollow">
-										<div class="icon-wrap">
-											<i class="fa fa-weibo"></i>
-										</div>
-									</a>
-									<a href="javascript:;" class="share-plain facebook style-plain" onclick="share('facebook');" rel="nofollow">
-										<div class="icon-wrap">
-											<i class="fa fa-facebook"></i>
-										</div>
-									</a>
-									<a href="javascript:;" class="share-plain googleplus style-plain" onclick="share('googleplus');" rel="nofollow">
-										<div class="icon-wrap">
-											<i class="fa fa-google-plus"></i>
-										</div>
-									</a>
-									<a href="javascript:;" class="share-plain weixin pop style-plain" rel="nofollow">
-										<div class="icon-wrap">
-											<i class="fa fa-weixin"></i>
-										</div>
-										<div class="share-int">
-											<div class="qrcode" data-url="<?php the_permalink() ?>"></div>
-											<p>打开微信“扫一扫”，打开网页后点击屏幕右上角分享按钮</p>
-										</div>
-									</a>
-								</div>
-								<script type="text/javascript">
-								function share(obj){
-									var qqShareURL="http://connect.qq.com/widget/shareqq/index.html?";
-									var weiboShareURL="http://service.weibo.com/share/share.php?";
-									var facebookShareURL="https://www.facebook.com/sharer/sharer.php?";
-									var twitterShareURL="https://twitter.com/intent/tweet?";
-									var googleplusShareURL="https://plus.google.com/share?";
-									var host_url="<?php the_permalink(); ?>";
-									var title="【<?php the_title(); ?>】";
-									var qqtitle="<?php the_title(); ?>";
-									var excerpt="<?php echo get_the_excerpt(); ?>";
-									var pic="<?php echo share_post_image(); ?>";
-									var _URL;
-									if(obj=="qq"){
-										_URL=qqShareURL+"url="+host_url+"&title="+qqtitle+"&pics="+pic+"&desc=&summary="+excerpt+"&site=vtrois";
-									}else if(obj=="weibo"){
-										_URL=weiboShareURL+"url="+host_url+"&title="+title+excerpt+"&pic="+pic;
-									}else if(obj=="facebook"){
-								 		_URL=facebookShareURL+"u="+host_url;
-									}else if(obj=="twitter"){
-								 		_URL=twitterShareURL+"text="+title+excerpt+"&url="+host_url;
-									}else if(obj=="googleplus"){
-								 		_URL=googleplusShareURL+"url="+host_url;
-									}
-									window.open(_URL);
-								}
-								</script>
-								</div>
+									<?php require_once( get_template_directory() . '/inc/share.php'); ?>
 								<?php endif; ?>
 					    		</div>
 						</footer>
