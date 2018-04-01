328b3edf0d7c51480cc99171e4046ffd47136883
SeatonJiang <seaton@vtrois.com>
2018-04-01 21:16:18 +0800

    [Fix] The default remove function #16

--- functions.php
+++ functions.php
@@ -94,10 +94,10 @@ add_action('wp_enqueue_scripts', 'kratos_theme_scripts');
  * @author Vtrois <seaton@vtrois.com>
  * @license GPL-3.0
  */
-remove_filter('the_content', 'wptexturize'); 
+add_filter( 'emoji_svg_url', '__return_false' );
+add_filter( 'show_admin_bar', '__return_false' );
 remove_action( 'wp_head', 'wp_print_head_scripts', 9 );
 remove_action( 'wp_head', 'wp_generator' );
-add_filter( 'show_admin_bar', '__return_false' );
 remove_action( 'wp_head', 'rsd_link' );
 remove_action( 'wp_head', 'wlwmanifest_link' );
 remove_action( 'wp_head', 'index_rel_link' );
@@ -109,13 +109,14 @@ remove_action( 'wp_head', 'feed_links', 2 );
 remove_action( 'wp_head', 'feed_links_extra', 3 );
 remove_action('admin_print_scripts', 'print_emoji_detection_script');
 remove_action('admin_print_styles', 'print_emoji_styles');
+remove_filter('the_content', 'wptexturize'); 
+remove_filter('comment_text', 'wptexturize');
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
 remove_action('embed_head', 'print_emoji_detection_script');
 remove_filter('the_content_feed', 'wp_staticize_emoji');
 remove_filter('comment_text_rss', 'wp_staticize_emoji');
 remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
-add_filter( 'emoji_svg_url', '__return_false' );
 
 
 add_action( 'wp_enqueue_scripts', 'mt_enqueue_scripts', 1 );
