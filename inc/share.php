5998a0f70059f8514ba159d4df91a5ccbad69578
Seaton Jiang <seaton@vtrois.com>
2019-03-28 18:26:24 +0800

    Remove google plus

--- inc/share.php
+++ inc/share.php
@@ -15,11 +15,6 @@
 				<i class="fa fa-facebook"></i>
 			</div>
 		</a>
-		<a href="javascript:;" class="share-plain googleplus style-plain" onclick="share('googleplus');" rel="nofollow">
-			<div class="icon-wrap">
-				<i class="fa fa-google-plus"></i>
-			</div>
-		</a>
 		<a href="javascript:;" class="share-plain weixin pop style-plain" rel="nofollow">
 			<div class="icon-wrap">
 				<i class="fa fa-weixin"></i>
@@ -36,7 +31,6 @@
 		var weiboShareURL="http://service.weibo.com/share/share.php?";
 		var facebookShareURL="https://www.facebook.com/sharer/sharer.php?";
 		var twitterShareURL="https://twitter.com/intent/tweet?";
-		var googleplusShareURL="https://plus.google.com/share?";
 		var host_url="<?php the_permalink(); ?>";
 		var title='<?php  echo str_replace("%22","%2522",rawurlencode('【'.get_the_title().'】')); ?>';
 		var qqtitle='<?php echo rawurlencode('【'.get_the_title().'】'); ?>';
@@ -52,8 +46,6 @@
 	 		_URL=facebookShareURL+"u="+host_url;
 		}else if(obj=="twitter"){
 	 		_URL=twitterShareURL+"text="+title+excerpt+"&url="+host_url;
-		}else if(obj=="googleplus"){
-	 		_URL=googleplusShareURL+"url="+host_url;
 		}
 		window.open(_URL);
 	}
