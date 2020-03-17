eea39c2ef34777ee5e42c8f6b5e3fa4cf1a73473
Seaton Jiang <seaton@vtrois.com>
2020-03-17 09:51:02 +0800

    style: format code with PSR2

--- functions.php
+++ functions.php
@@ -3,37 +3,37 @@
  * 模板函数
  * @author Seaton Jiang <seaton@vtrois.com>
  * @license MIT License
- * @version 2020.02.15
+ * @version 2020.03.17
  */
 
 define('THEME_VERSION', wp_get_theme()->get('Version'));
 
 // 主题配置
-require get_template_directory() .'/inc/options-framework/autoload.php';
+require get_template_directory() . '/inc/options-framework/autoload.php';
 
 // 更新配置
-require get_template_directory() .'/inc/update-checker/autoload.php';
+require get_template_directory() . '/inc/update-checker/autoload.php';
 
 // 核心配置
-require get_template_directory() .'/inc/theme-core.php';
+require get_template_directory() . '/inc/theme-core.php';
 
 // 站点配置
-require get_template_directory() .'/inc/theme-setting.php';
+require get_template_directory() . '/inc/theme-setting.php';
 
 // 文章配置
-require get_template_directory() .'/inc/theme-article.php';
+require get_template_directory() . '/inc/theme-article.php';
 
 // 小工具配置
-require get_template_directory() .'/inc/theme-widgets.php';
+require get_template_directory() . '/inc/theme-widgets.php';
 
 // 文章增强
-require get_template_directory() .'/inc/theme-shortcode.php';
+require get_template_directory() . '/inc/theme-shortcode.php';
 
 // 添加导航目录
-require get_template_directory() .'/inc/theme-navwalker.php';
+require get_template_directory() . '/inc/theme-navwalker.php';
 
 // SMTP 配置
-require get_template_directory() .'/inc/theme-smtp.php';
+require get_template_directory() . '/inc/theme-smtp.php';
 
 // 自定义函数
-require get_template_directory() .'/custom/custom.php';
+require get_template_directory() . '/custom/custom.php';
