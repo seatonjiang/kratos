ae5a76a8c01bce108729ba6e8426e9d73aeb5436
Seaton Jiang <seaton@vtrois.com>
2020-06-06 09:17:55 +0800

    chore: canonical code format

--- assets/js/buttons/more.js
+++ assets/js/buttons/more.js
@@ -1,273 +1,273 @@
-(function() {
+(function () {
     tinymce.create('tinymce.plugins.h2title', {
-        init : function(ed, url) {
+        init: function (ed, url) {
             ed.addButton('h2title', {
-                title : '特色标题',
-                image : url+'/images/title.png',
-                onclick : function() {
-                     ed.selection.setContent('[h2title]' + ed.selection.getContent() + '[/h2title]');
+                title: '特色标题',
+                image: url + '/images/title.png',
+                onclick: function () {
+                    ed.selection.setContent('[h2title]' + ed.selection.getContent() + '[/h2title]')
                 }
-            });
+            })
         },
-        createControl : function(n, cm) {
-            return null;
+        createControl: function (n, cm) {
+            return null
         },
-    });
-    tinymce.PluginManager.add('h2title', tinymce.plugins.h2title);
+    })
+    tinymce.PluginManager.add('h2title', tinymce.plugins.h2title)
 
     tinymce.create('tinymce.plugins.success', {
-        init : function(ed, url) {
+        init: function (ed, url) {
             ed.addButton('success', {
-                title : '绿色背景栏',
-                image : url+'/images/success.png',
-                onclick : function() {
-                     ed.selection.setContent('[success]' + ed.selection.getContent() + '[/success]');
+                title: '绿色背景栏',
+                image: url + '/images/success.png',
+                onclick: function () {
+                    ed.selection.setContent('[success]' + ed.selection.getContent() + '[/success]')
                 }
-            });
+            })
         },
-        createControl : function(n, cm) {
-            return null;
+        createControl: function (n, cm) {
+            return null
         },
-    });
-    tinymce.PluginManager.add('success', tinymce.plugins.success);
+    })
+    tinymce.PluginManager.add('success', tinymce.plugins.success)
 
     tinymce.create('tinymce.plugins.info', {
-        init : function(ed, url) {
+        init: function (ed, url) {
             ed.addButton('info', {
-                title : '蓝色背景栏',
-                image : url+'/images/info.png',
-                onclick : function() {
-                     ed.selection.setContent('[info]' + ed.selection.getContent() + '[/info]');
+                title: '蓝色背景栏',
+                image: url + '/images/info.png',
+                onclick: function () {
+                    ed.selection.setContent('[info]' + ed.selection.getContent() + '[/info]')
                 }
-            });
+            })
         },
-        createControl : function(n, cm) {
-            return null;
+        createControl: function (n, cm) {
+            return null
         },
-    });
-    tinymce.PluginManager.add('info', tinymce.plugins.info);
+    })
+    tinymce.PluginManager.add('info', tinymce.plugins.info)
 
     tinymce.create('tinymce.plugins.warning', {
-        init : function(ed, url) {
+        init: function (ed, url) {
             ed.addButton('warning', {
-                title : '黄色背景栏',
-                image : url+'/images/warning.png',
-                onclick : function() {
-                     ed.selection.setContent('[warning]' + ed.selection.getContent() + '[/warning]');
+                title: '黄色背景栏',
+                image: url + '/images/warning.png',
+                onclick: function () {
+                    ed.selection.setContent('[warning]' + ed.selection.getContent() + '[/warning]')
                 }
-            });
+            })
         },
-        createControl : function(n, cm) {
-            return null;
+        createControl: function (n, cm) {
+            return null
         },
-    });
-    tinymce.PluginManager.add('warning', tinymce.plugins.warning);
+    })
+    tinymce.PluginManager.add('warning', tinymce.plugins.warning)
 
     tinymce.create('tinymce.plugins.danger', {
-        init : function(ed, url) {
+        init: function (ed, url) {
             ed.addButton('danger', {
-                title : '红色背景栏',
-                image : url+'/images/danger.png',
-                onclick : function() {
-                     ed.selection.setContent('[danger]' + ed.selection.getContent() + '[/danger]');
+                title: '红色背景栏',
+                image: url + '/images/danger.png',
+                onclick: function () {
+                    ed.selection.setContent('[danger]' + ed.selection.getContent() + '[/danger]')
                 }
-            });
+            })
         },
-        createControl : function(n, cm) {
-            return null;
+        createControl: function (n, cm) {
+            return null
         },
-    });
-    tinymce.PluginManager.add('danger', tinymce.plugins.danger);     
+    })
+    tinymce.PluginManager.add('danger', tinymce.plugins.danger)
 
     tinymce.create('tinymce.plugins.successbox', {
-        init : function(ed, url) {
+        init: function (ed, url) {
             ed.addButton('successbox', {
-                title : '绿色面板',
-                image : url+'/images/successbox.png',
-                onclick : function() {
-                     ed.selection.setContent('[successbox title="标题内容"]' + ed.selection.getContent() + '[/successbox]');
+                title: '绿色面板',
+                image: url + '/images/successbox.png',
+                onclick: function () {
+                    ed.selection.setContent('[successbox title="标题内容"]' + ed.selection.getContent() + '[/successbox]')
                 }
-            });
+            })
         },
-        createControl : function(n, cm) {
-            return null;
+        createControl: function (n, cm) {
+            return null
         },
-    });
-    tinymce.PluginManager.add('successbox', tinymce.plugins.successbox);
+    })
+    tinymce.PluginManager.add('successbox', tinymce.plugins.successbox)
 
     tinymce.create('tinymce.plugins.infoboxs', {
-        init : function(ed, url) {
+        init: function (ed, url) {
             ed.addButton('infoboxs', {
-                title : '蓝色面板',
-                image : url+'/images/infobox.png',
-                onclick : function() {
-                     ed.selection.setContent('[infobox title="标题内容"]' + ed.selection.getContent() + '[/infobox]');
+                title: '蓝色面板',
+                image: url + '/images/infobox.png',
+                onclick: function () {
+                    ed.selection.setContent('[infobox title="标题内容"]' + ed.selection.getContent() + '[/infobox]')
                 }
-            });
+            })
         },
-        createControl : function(n, cm) {
-            return null;
+        createControl: function (n, cm) {
+            return null
         },
-    });
-    tinymce.PluginManager.add('infoboxs', tinymce.plugins.infoboxs);
+    })
+    tinymce.PluginManager.add('infoboxs', tinymce.plugins.infoboxs)
 
     tinymce.create('tinymce.plugins.warningbox', {
-        init : function(ed, url) {
+        init: function (ed, url) {
             ed.addButton('warningbox', {
-                title : '黄色面板',
-                image : url+'/images/warningbox.png',
-                onclick : function() {
-                     ed.selection.setContent('[warningbox title="标题内容"]' + ed.selection.getContent() + '[/warningbox]');
+                title: '黄色面板',
+                image: url + '/images/warningbox.png',
+                onclick: function () {
+                    ed.selection.setContent('[warningbox title="标题内容"]' + ed.selection.getContent() + '[/warningbox]')
                 }
-            });
+            })
         },
-        createControl : function(n, cm) {
-            return null;
+        createControl: function (n, cm) {
+            return null
         },
-    });
-    tinymce.PluginManager.add('warningbox', tinymce.plugins.warningbox);
+    })
+    tinymce.PluginManager.add('warningbox', tinymce.plugins.warningbox)
 
     tinymce.create('tinymce.plugins.dangerbox', {
-        init : function(ed, url) {
+        init: function (ed, url) {
             ed.addButton('dangerbox', {
-                title : '红色面板',
-                image : url+'/images/dangerbox.png',
-                onclick : function() {
-                     ed.selection.setContent('[dangerbox title="标题内容"]' + ed.selection.getContent() + '[/dangerbox]');
+                title: '红色面板',
+                image: url + '/images/dangerbox.png',
+                onclick: function () {
+                    ed.selection.setContent('[dangerbox title="标题内容"]' + ed.selection.getContent() + '[/dangerbox]')
                 }
-            });
+            })
         },
-        createControl : function(n, cm) {
-            return null;
+        createControl: function (n, cm) {
+            return null
         },
-    });
-    tinymce.PluginManager.add('dangerbox', tinymce.plugins.dangerbox);        
+    })
+    tinymce.PluginManager.add('dangerbox', tinymce.plugins.dangerbox)
 
     tinymce.create('tinymce.plugins.kbd', {
-        init : function(ed, url) {
+        init: function (ed, url) {
             ed.addButton('kbd', {
-                title : '键盘文本',
-                image : url+'/images/kbd.png',
-                onclick : function() {
-                     ed.selection.setContent('[kbd]' + ed.selection.getContent() + '[/kbd]');
+                title: '键盘文本',
+                image: url + '/images/kbd.png',
+                onclick: function () {
+                    ed.selection.setContent('[kbd]' + ed.selection.getContent() + '[/kbd]')
                 }
-            });
+            })
         },
-        createControl : function(n, cm) {
-            return null;
+        createControl: function (n, cm) {
+            return null
         },
-    });
-    tinymce.PluginManager.add('kbd', tinymce.plugins.kbd);
+    })
+    tinymce.PluginManager.add('kbd', tinymce.plugins.kbd)
 
     tinymce.create('tinymce.plugins.mark', {
-        init : function(ed, url) {
+        init: function (ed, url) {
             ed.addButton('mark', {
-                title : '内容标记',
-                image : url+'/images/mark.png',
-                onclick : function() {
-                     ed.selection.setContent('[mark]' + ed.selection.getContent() + '[/mark]');
+                title: '内容标记',
+                image: url + '/images/mark.png',
+                onclick: function () {
+                    ed.selection.setContent('[mark]' + ed.selection.getContent() + '[/mark]')
                 }
-            });
+            })
         },
-        createControl : function(n, cm) {
-            return null;
+        createControl: function (n, cm) {
+            return null
         },
-    });
-    tinymce.PluginManager.add('mark', tinymce.plugins.mark);
+    })
+    tinymce.PluginManager.add('mark', tinymce.plugins.mark)
 
     tinymce.create('tinymce.plugins.striped', {
-        init : function(ed, url) {
+        init: function (ed, url) {
             ed.addButton('striped', {
-                title : '进度条',
-                image : url+'/images/striped.png',
-                onclick : function() {
-                     ed.selection.setContent('[striped]' + ed.selection.getContent() + '[/striped]');
+                title: '进度条',
+                image: url + '/images/striped.png',
+                onclick: function () {
+                    ed.selection.setContent('[striped]' + ed.selection.getContent() + '[/striped]')
                 }
-            });
+            })
         },
-        createControl : function(n, cm) {
-            return null;
+        createControl: function (n, cm) {
+            return null
         },
-    });
-    tinymce.PluginManager.add('striped', tinymce.plugins.striped);
+    })
+    tinymce.PluginManager.add('striped', tinymce.plugins.striped)
 
     tinymce.create('tinymce.plugins.bdbtn', {
-        init : function(ed, url) {
+        init: function (ed, url) {
             ed.addButton('bdbtn', {
-                title : '下载按钮',
-                image : url+'/images/bdbtn.png',
-                onclick : function() {
-                     ed.selection.setContent('[bdbtn]' + ed.selection.getContent() + '[/bdbtn]');
+                title: '下载按钮',
+                image: url + '/images/bdbtn.png',
+                onclick: function () {
+                    ed.selection.setContent('[bdbtn]' + ed.selection.getContent() + '[/bdbtn]')
                 }
-            });
+            })
         },
-        createControl : function(n, cm) {
-            return null;
+        createControl: function (n, cm) {
+            return null
         },
-    });
-    tinymce.PluginManager.add('bdbtn', tinymce.plugins.bdbtn);
+    })
+    tinymce.PluginManager.add('bdbtn', tinymce.plugins.bdbtn)
 
     tinymce.create('tinymce.plugins.music', {
-        init : function(ed, url) {
+        init: function (ed, url) {
             ed.addButton('music', {
-                title : '网易云音乐',
-                image : url+'/images/music.png',
-                onclick : function() {
-                     ed.selection.setContent('[music]' + ed.selection.getContent() + '[/music]');
+                title: '网易云音乐',
+                image: url + '/images/music.png',
+                onclick: function () {
+                    ed.selection.setContent('[music]' + ed.selection.getContent() + '[/music]')
                 }
-            });
+            })
         },
-        createControl : function(n, cm) {
-            return null;
+        createControl: function (n, cm) {
+            return null
         },
-    });
-    tinymce.PluginManager.add('music', tinymce.plugins.music);
+    })
+    tinymce.PluginManager.add('music', tinymce.plugins.music)
 
     tinymce.create('tinymce.plugins.vqq', {
-        init : function(ed, url) {
+        init: function (ed, url) {
             ed.addButton('vqq', {
-                title : '腾讯视频',
-                image : url+'/images/vqq.png',
-                onclick : function() {
-                     ed.selection.setContent('[vqq]' + ed.selection.getContent() + '[/vqq]');
+                title: '腾讯视频',
+                image: url + '/images/vqq.png',
+                onclick: function () {
+                    ed.selection.setContent('[vqq]' + ed.selection.getContent() + '[/vqq]')
                 }
-            });
+            })
         },
-        createControl : function(n, cm) {
-            return null;
+        createControl: function (n, cm) {
+            return null
         },
-    });
-    tinymce.PluginManager.add('vqq', tinymce.plugins.vqq);
+    })
+    tinymce.PluginManager.add('vqq', tinymce.plugins.vqq)
 
     tinymce.create('tinymce.plugins.bilibili', {
-        init : function(ed, url) {
+        init: function (ed, url) {
             ed.addButton('bilibili', {
-                title : '哔哩哔哩',
-                image : url+'/images/bilibili.png',
-                onclick : function() {
-                     ed.selection.setContent('[bilibili cid=""]' + ed.selection.getContent() + '[/bilibili]');
+                title: '哔哩哔哩',
+                image: url + '/images/bilibili.png',
+                onclick: function () {
+                    ed.selection.setContent('[bilibili cid=""]' + ed.selection.getContent() + '[/bilibili]')
                 }
-            });
+            })
         },
-        createControl : function(n, cm) {
-            return null;
+        createControl: function (n, cm) {
+            return null
         },
-    });
-    tinymce.PluginManager.add('bilibili', tinymce.plugins.bilibili);
+    })
+    tinymce.PluginManager.add('bilibili', tinymce.plugins.bilibili)
 
     tinymce.create('tinymce.plugins.youtube', {
-        init : function(ed, url) {
+        init: function (ed, url) {
             ed.addButton('youtube', {
-                title : 'YouTube',
-                image : url+'/images/youtube.png',
-                onclick : function() {
-                     ed.selection.setContent('[youtube]' + ed.selection.getContent() + '[/youtube]');
+                title: 'YouTube',
+                image: url + '/images/youtube.png',
+                onclick: function () {
+                    ed.selection.setContent('[youtube]' + ed.selection.getContent() + '[/youtube]')
                 }
-            });
+            })
         },
-        createControl : function(n, cm) {
-            return null;
+        createControl: function (n, cm) {
+            return null
         },
-    });
-    tinymce.PluginManager.add('youtube', tinymce.plugins.youtube); 
+    })
+    tinymce.PluginManager.add('youtube', tinymce.plugins.youtube)
 })();
--- assets/js/kratos.js
+++ assets/js/kratos.js
@@ -1,171 +1,171 @@
 /*!
- * Kratos v3.0.9
+ * Kratos
  * Seaton Jiang <seaton@vtrois.com>
  */
-;(function() {
-	'use strict';
+; (function () {
+	'use strict'
 
-	var navbarConfig = function() {
-		$('#navbutton').on('click', function() {
-			$('.navbar-toggler').toggleClass('nav-close');
-		});
-	};
+	var navbarConfig = function () {
+		$('#navbutton').on('click', function () {
+			$('.navbar-toggler').toggleClass('nav-close')
+		})
+	}
 
-	var tooltipConfig = function(){
+	var tooltipConfig = function () {
 		$(function () {
-		  $('[data-toggle="tooltip"]').tooltip()
+			$('[data-toggle="tooltip"]').tooltip()
 		})
-	};
+	}
 
-	var gotopConfig = function() {
-		$(window).on('load', function() {
-			var $win = $(window);
+	var gotopConfig = function () {
+		$(window).on('load', function () {
+			var $win = $(window)
 			if ($win.scrollTop() > 200) {
-				$('.gotop').addClass('active');
+				$('.gotop').addClass('active')
 			} else {
-				$('.gotop').removeClass('active');
+				$('.gotop').removeClass('active')
 			}
-			$win.scroll(function() {
+			$win.scroll(function () {
 				if ($win.scrollTop() > 200) {
-					$('.gotop').addClass('active');
+					$('.gotop').addClass('active')
 				} else {
-					$('.gotop').removeClass('active');
+					$('.gotop').removeClass('active')
 				}
-			});
+			})
 		})
-		$('.gotop').on('click', function(event) {
-			event.preventDefault();
+		$('.gotop').on('click', function (event) {
+			event.preventDefault()
 			$('html, body').animate({
 				scrollTop: $('html').offset().top
-			}, 500);
-			return false;
-		});
+			}, 500)
+			return false
+		})
 	}
 
-	var searchConfig = function() {
-		$('.search').on('click', function(e) {
+	var searchConfig = function () {
+		$('.search').on('click', function (e) {
 			$('.search-form').animate({
 				width: '200px'
-			}, 200), $('.search-form input').css('display', 'block');
-			$(document).one('click', function() {
+			}, 200), $('.search-form input').css('display', 'block')
+			$(document).one('click', function () {
 				$('.search-form').animate({
 					width: '0'
-				}, 100), $('.search-form input').hide();
-			});
-			e.stopPropagation();
-		});
-		$('.search-form').on('click', function(e) {
-			e.stopPropagation();
+				}, 100), $('.search-form input').hide()
+			})
+			e.stopPropagation()
+		})
+		$('.search-form').on('click', function (e) {
+			e.stopPropagation()
 		})
 	}
 
-	var wechatConfig = function() {
-		$('.wechat').mouseout(function() {
-			$('.wechat-pic')[0].style.display = 'none';
+	var wechatConfig = function () {
+		$('.wechat').mouseout(function () {
+			$('.wechat-pic')[0].style.display = 'none'
 		})
-		$('.wechat').mouseover(function() {
-			$('.wechat-pic')[0].style.display = 'block';
+		$('.wechat').mouseover(function () {
+			$('.wechat-pic')[0].style.display = 'block'
 		})
 	}
 
-	var smiliesConfig = function() {
-		$('#addsmile').on("click", function(e) {
-			$('.smile').toggleClass('open');
-			$(document).one("click", function() {
-				$('.smile').toggleClass('open');
-			});
-			e.stopPropagation();
-			return false;
-		});
+	var smiliesConfig = function () {
+		$('#addsmile').on("click", function (e) {
+			$('.smile').toggleClass('open')
+			$(document).one("click", function () {
+				$('.smile').toggleClass('open')
+			})
+			e.stopPropagation()
+			return false
+		})
 	}
 
-	var postlikeConfig = function() {
-		$.fn.postLike = function() {
+	var postlikeConfig = function () {
+		$.fn.postLike = function () {
 			if ($(this).hasClass('done')) {
-				layer.msg(kratos.repeat, function() {});
-				return false;
+				layer.msg(kratos.repeat, function () { })
+				return false
 			} else {
-				$(this).addClass('done');
-				layer.msg(kratos.thanks);
+				$(this).addClass('done')
+				layer.msg(kratos.thanks)
 				var id = $(this).data("id"),
-					action = $(this).data('action');
+					action = $(this).data('action')
 				var ajax_data = {
 					action: "love",
 					um_id: id,
 					um_action: action
-				};
-				$.post( kratos.site+'/wp-admin/admin-ajax.php', ajax_data, function(data) {});
-				return false;
+				}
+				$.post(kratos.site + '/wp-admin/admin-ajax.php', ajax_data, function (data) { })
+				return false
 			}
-		};
-		$(document).on("click", ".btn-thumbs", function() {
-			$(this).postLike();
-		});
-	};
+		}
+		$(document).on("click", ".btn-thumbs", function () {
+			$(this).postLike()
+		})
+	}
 
-	var donateConfig = function(){
-		$("#donate").on('click', function(){
+	var donateConfig = function () {
+		$("#donate").on('click', function () {
 			layer.open({
 				type: 1,
 				area: ['300px', '370px'],
-				title:kratos.donate,
-				resize:false,
+				title: kratos.donate,
+				resize: false,
 				scrollbar: false,
-				content: '<div class="donate-box"><div class="meta-pay text-center my-2"><strong>'+kratos.scan+'</strong></div><div class="qr-pay text-center"><img class="pay-img" id="alipay_qr" src="'+kratos.alipay+'"><img class="pay-img d-none" id="wechat_qr" src="'+kratos.wechat+'"></div><div class="choose-pay text-center mt-2"><input id="alipay" type="radio" name="pay-method" checked><label for="alipay" class="pay-button"><img src="'+kratos.directory+'/assets/img/payment/alipay.png"></label><input id="wechatpay" type="radio" name="pay-method"><label for="wechatpay" class="pay-button"><img src="'+kratos.directory+'/assets/img/payment/wechat.png"></label></div></div>'
-			});
-			$(".choose-pay input[type='radio']").click(function(){
-				var id= $(this).attr("id");
-				if (id=='alipay') { $(".qr-pay #alipay_qr").removeClass('d-none');$(".qr-pay #wechat_qr").addClass('d-none') };
-				if (id=='wechatpay') { $(".qr-pay #alipay_qr").addClass('d-none');$(".qr-pay #wechat_qr").removeClass('d-none') };
-			});
-		});
-	};
-
-	var consoleConfig = function(){
-		console.log("\n         ▄▄▄▄▄▄▄  ▄    ▄▄▄▄▄▄  ▄▄▄▄▄▄▄\n         █ ▄▄▄ █ ▀█▄█▀ █▀ ▀ ▄  █ ▄▄▄ █\n         █ ███ █ ▀▀▄▀▄▀▄▀██▄▄█ █ ███ █\n         █▄▄▄▄▄█ █ █ ▄▀▄▀█ ▄ █ █▄▄▄▄▄█\n         ▄▄▄▄▄ ▄▄▄▄▄ █▄▄█▄▄ █▀▄ ▄ ▄ ▄ \n          ▀▀█ ▄▄███ ▄▄ ▀▄ ▀█▀▄▀███   ▀\n          ▀█ ▄█▄▀ █▀█ ▄ ▄▄▄ ▄▀█▀  ▄▄▀ \n          █ ▀▄█▄▄█▄▀ ▀▀▀█▀█▀▀█▀█▀█ █ ▀\n         ▀▄█ ▀ ▄▄▄▄█ █▄▀▀▀ ▀█▀▀▀▄  ▄▀ \n         █ ▄▄▄ ▄ ▄▀█▄▄▄▀▄█▄▄▀█▄▀████ ▀\n         █ ███ ▄▀▄▄ █ ▄█▄ ▄ ▀▄▄▄▄▄ ▄█▄\n         ▄▄▄▄▄▄▄ █▄  ▀▀▀██▀▄▄█ ▄ ███▀▀\n         █ ▄▄▄ █ ▄▄  █ ██▀▄ ▀█▄▄▄██▄▄█\n         █ ███ █ █ ▀▄▄▀ █▀▄▀▄█ ▄▄▄█▄█▀\n         █▄▄▄▄▄█ ████▄ ▀ ▀▄  ▀▀ ██▀▄▀ \n\n██╗  ██╗██████╗  █████╗ ████████╗ ██████╗ ███████╗\n██║ ██╔╝██╔══██╗██╔══██╗╚══██╔══╝██╔═══██╗██╔════╝\n█████╔╝ ██████╔╝███████║   ██║   ██║   ██║███████╗\n██╔═██╗ ██╔══██╗██╔══██║   ██║   ██║   ██║╚════██║\n██║  ██╗██║  ██║██║  ██║   ██║   ╚██████╔╝███████║\n╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚══════╝\n");
+				content: '<div class="donate-box"><div class="meta-pay text-center my-2"><strong>' + kratos.scan + '</strong></div><div class="qr-pay text-center"><img class="pay-img" id="alipay_qr" src="' + kratos.alipay + '"><img class="pay-img d-none" id="wechat_qr" src="' + kratos.wechat + '"></div><div class="choose-pay text-center mt-2"><input id="alipay" type="radio" name="pay-method" checked><label for="alipay" class="pay-button"><img src="' + kratos.directory + '/assets/img/payment/alipay.png"></label><input id="wechatpay" type="radio" name="pay-method"><label for="wechatpay" class="pay-button"><img src="' + kratos.directory + '/assets/img/payment/wechat.png"></label></div></div>'
+			})
+			$(".choose-pay input[type='radio']").click(function () {
+				var id = $(this).attr("id")
+				if (id == 'alipay') { $(".qr-pay #alipay_qr").removeClass('d-none'); $(".qr-pay #wechat_qr").addClass('d-none') };
+				if (id == 'wechatpay') { $(".qr-pay #alipay_qr").addClass('d-none'); $(".qr-pay #wechat_qr").removeClass('d-none') };
+			})
+		})
 	}
 
-	$(function() {
-		navbarConfig();
-		tooltipConfig();
-		gotopConfig();
-		searchConfig();
-		wechatConfig();
-		smiliesConfig();
-		postlikeConfig();
-		donateConfig();
-		consoleConfig();
-	});
-}());
+	var consoleConfig = function () {
+		console.log("\n         ▄▄▄▄▄▄▄  ▄    ▄▄▄▄▄▄  ▄▄▄▄▄▄▄\n         █ ▄▄▄ █ ▀█▄█▀ █▀ ▀ ▄  █ ▄▄▄ █\n         █ ███ █ ▀▀▄▀▄▀▄▀██▄▄█ █ ███ █\n         █▄▄▄▄▄█ █ █ ▄▀▄▀█ ▄ █ █▄▄▄▄▄█\n         ▄▄▄▄▄ ▄▄▄▄▄ █▄▄█▄▄ █▀▄ ▄ ▄ ▄ \n          ▀▀█ ▄▄███ ▄▄ ▀▄ ▀█▀▄▀███   ▀\n          ▀█ ▄█▄▀ █▀█ ▄ ▄▄▄ ▄▀█▀  ▄▄▀ \n          █ ▀▄█▄▄█▄▀ ▀▀▀█▀█▀▀█▀█▀█ █ ▀\n         ▀▄█ ▀ ▄▄▄▄█ █▄▀▀▀ ▀█▀▀▀▄  ▄▀ \n         █ ▄▄▄ ▄ ▄▀█▄▄▄▀▄█▄▄▀█▄▀████ ▀\n         █ ███ ▄▀▄▄ █ ▄█▄ ▄ ▀▄▄▄▄▄ ▄█▄\n         ▄▄▄▄▄▄▄ █▄  ▀▀▀██▀▄▄█ ▄ ███▀▀\n         █ ▄▄▄ █ ▄▄  █ ██▀▄ ▀█▄▄▄██▄▄█\n         █ ███ █ █ ▀▄▄▀ █▀▄▀▄█ ▄▄▄█▄█▀\n         █▄▄▄▄▄█ ████▄ ▀ ▀▄  ▀▀ ██▀▄▀ \n\n██╗  ██╗██████╗  █████╗ ████████╗ ██████╗ ███████╗\n██║ ██╔╝██╔══██╗██╔══██╗╚══██╔══╝██╔═══██╗██╔════╝\n█████╔╝ ██████╔╝███████║   ██║   ██║   ██║███████╗\n██╔═██╗ ██╔══██╗██╔══██║   ██║   ██║   ██║╚════██║\n██║  ██╗██║  ██║██║  ██║   ██║   ╚██████╔╝███████║\n╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚══════╝\n")
+	}
 
-function grin(tag) {
-    var myField;
-    tag = ' ' + tag + ' ';
-    if (document.getElementById('comment') && document.getElementById('comment').type == 'textarea') {
-        myField = document.getElementById('comment');
-    } else {
-        return false;
-    }
-    if (document.selection) {
-        myField.focus();
-        sel = document.selection.createRange();
-        sel.text = tag;
-        myField.focus();
-    }
-    else if (myField.selectionStart || myField.selectionStart == '0') {
-        var startPos = myField.selectionStart;
-        var endPos = myField.selectionEnd;
-        var cursorPos = endPos;
-        myField.value = myField.value.substring(0, startPos)
-                      + tag
-                      + myField.value.substring(endPos, myField.value.length);
-        cursorPos += tag.length;
-        myField.focus();
-        myField.selectionStart = cursorPos;
-        myField.selectionEnd = cursorPos;
-    }
-    else {
-        myField.value += tag;
-        myField.focus();
-    }
-};
+	$(function () {
+		navbarConfig()
+		tooltipConfig()
+		gotopConfig()
+		searchConfig()
+		wechatConfig()
+		smiliesConfig()
+		postlikeConfig()
+		donateConfig()
+		consoleConfig()
+	})
+}())
+
+function grin (tag) {
+	var myField
+	tag = ' ' + tag + ' '
+	if (document.getElementById('comment') && document.getElementById('comment').type == 'textarea') {
+		myField = document.getElementById('comment')
+	} else {
+		return false
+	}
+	if (document.selection) {
+		myField.focus()
+		sel = document.selection.createRange()
+		sel.text = tag
+		myField.focus()
+	}
+	else if (myField.selectionStart || myField.selectionStart == '0') {
+		var startPos = myField.selectionStart
+		var endPos = myField.selectionEnd
+		var cursorPos = endPos
+		myField.value = myField.value.substring(0, startPos)
+			+ tag
+			+ myField.value.substring(endPos, myField.value.length)
+		cursorPos += tag.length
+		myField.focus()
+		myField.selectionStart = cursorPos
+		myField.selectionEnd = cursorPos
+	}
+	else {
+		myField.value += tag
+		myField.focus()
+	}
+}
