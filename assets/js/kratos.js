da391e4aa342240aa5f51a0daa4a75ff8bc38d03
Seaton Jiang <seaton@vtrois.com>
2021-02-07 16:40:38 +0800

    feat: upgrade to version 3.2.6

--- assets/js/kratos.js
+++ assets/js/kratos.js
@@ -5,7 +5,7 @@
 (function () {
   "use strict";
 
-  var KRATOS_VERSION = "3.2.5";
+  var KRATOS_VERSION = "3.2.6";
 
   var navbarConfig = function () {
     $("#navbutton").on("click", function () {
