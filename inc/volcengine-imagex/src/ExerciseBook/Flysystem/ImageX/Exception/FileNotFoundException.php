a0895e5bb00ab17a537cfb690d8ee2e27e07f188
Seaton Jiang <seatonjiang@vtrois.com>
2021-08-20 12:09:38 +0800

    refactor: optimize volcengine function

---
+++ inc/volcengine-imagex/src/ExerciseBook/Flysystem/ImageX/Exception/FileNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace ExerciseBook\Flysystem\ImageX\Exception;
+
+
+
+class FileNotFoundException extends FilesystemException
+{
+
+
+}
---
+++ inc/volcengine-imagex/src/ExerciseBook/Flysystem/ImageX/ImageXConfig.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ExerciseBook\Flysystem\ImageX;
+
+class ImageXConfig
+{
+    /**
+     * @var string 地域
+     */
+    public $region;
+
+    /**
+     * @var string
+     */
+    public $accessKey;
+
+    /**
+     * @var string
+     */
+    public $secretKey;
+
+    /**
+     * @var string 服务编号
+     */
+    public $serviceId;
+
+    /**
+     * @var string 绑定域名
+     */
+    public $domain;
+}
