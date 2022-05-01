bbf16b417ae9abc288735e67dd9dbda853a29acf
Seaton Jiang <hi@seatonjiang.com>
2022-05-02 00:33:35 +0800

    feat: upgrade to version 4.1.3

--- assets/js/kratos.js
+++ assets/js/kratos.js
@@ -5,7 +5,7 @@
 ;(function () {
     'use strict'
 
-    var KRATOS_VERSION = '4.1.2'
+    var KRATOS_VERSION = '4.1.3'
 
     var navbarConfig = function () {
         $('#navbutton').on('click', function () {
--- functions.php
+++ functions.php
@@ -4,10 +4,10 @@
  * 模板函数
  * @author Seaton Jiang <hi@seatonjiang.com>
  * @license GPL-3.0 License
- * @version 2022.02.20
+ * @version 2022.05.02
  */
 
-define('THEME_VERSION', '4.1.2');
+define('THEME_VERSION', '4.1.3');
 
 // 主题配置
 require get_template_directory() . '/inc/codestar-framework/autoload.php';
