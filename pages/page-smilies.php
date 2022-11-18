seatonjiang/kratos

commit 655d3eb475bb2147bc90dbac89374714fee12aed

refactor: optimize code structure (#545)

pages/page-smilies.php
@@ -6,25 +6,30 @@
  * @license GPL-3.0 License
  * @version 2022.01.26
  */
-$smilies = '
-<a href="javascript:grin(\':razz:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/razz.png" alt="" class="d-block"/></a>
-<a href="javascript:grin(\':evil:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/evil.png" alt="" class="d-block"/></a>
-<a href="javascript:grin(\':exclaim:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/exclaim.png" alt="" class="d-block"/></a>
-<a href="javascript:grin(\':smile:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/smile.png" alt="" class="d-block"/></a>
-<a href="javascript:grin(\':redface:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/redface.png" alt="" class="d-block"/></a>
-<a href="javascript:grin(\':biggrin:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/biggrin.png" alt="" class="d-block"/></a>
-<a href="javascript:grin(\':eek:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/eek.png" alt="" class="d-block"/></a>
-<a href="javascript:grin(\':confused:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/confused.png" alt="" class="d-block"/></a>
-<a href="javascript:grin(\':idea:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/idea.png" alt="" class="d-block"/></a>
-<a href="javascript:grin(\':lol:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/lol.png" alt="" class="d-block"/></a>
-<a href="javascript:grin(\':mad:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/mad.png" alt="" class="d-block"/></a>
-<a href="javascript:grin(\':twisted:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/twisted.png" alt="" class="d-block"/></a>
-<a href="javascript:grin(\':rolleyes:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/rolleyes.png" alt="" class="d-block"/></a>
-<a href="javascript:grin(\':wink:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/wink.png" alt="" class="d-block"/></a>
-<a href="javascript:grin(\':cool:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/cool.png" alt="" class="d-block"/></a>
-<a href="javascript:grin(\':arrow:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/arrow.png" alt="" class="d-block"/></a>
-<a href="javascript:grin(\':neutral:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/neutral.png" alt="" class="d-block"/></a>
-<a href="javascript:grin(\':cry:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/cry.png" alt="" class="d-block"/></a>
-<a href="javascript:grin(\':mrgreen:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/mrgreen.png" alt="" class="d-block"/></a>
-<a href="javascript:grin(\':drooling:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/drooling.png" alt="" class="d-block"/></a>
-<a href="javascript:grin(\':persevering:\')"><img src="' . ASSET_PATH . '/assets/img/smilies/persevering.png" alt="" class="d-block"/></a>';
+$smilies = join("\n", array_map(function ($s) {
+    return '<a href="javascript:grin(\':' . $s . ':\')"><img src="'
+        . apply_filter('smilies_src', ASSET_PATH . "/assets/img/smilies/{$s}.png", "{$s}.png", site_url())
+        . '" alt="' . $s . '" class="d-block"/></a>';
+}, [
+    'razz',
+    'evil',
+    'exclaim',
+    'smile',
+    'redface',
+    'biggrin',
+    'eek',
+    'confused',
+    'idea',
+    'lol',
+    'mad',
+    'twisted',
+    'rolleyes',
+    'wink',
+    'cool',
+    'arrow',
+    'neutral',
+    'cry',
+    'mrgreen',
+    'drooling',
+    'persevering'
+]));

commit b6e105d97ec980c2902786aeb75ee6515811019b

fix another typo which introduced in #545

pages/page-smilies.php
@@ -7,9 +7,9 @@
  * @version 2022.01.26
  */
 $smilies = join("\n", array_map(function ($s) {
-    return '<a href="javascript:grin(\':' . $s . ':\')"><img src="'
-        . apply_filter('smilies_src', ASSET_PATH . "/assets/img/smilies/{$s}.png", "{$s}.png", site_url())
-        . '" alt="' . $s . '" class="d-block"/></a>';
+    return '<a href="javascript:grin(\':' . $s . ':\')"><img class="d-block" src="'
+        . apply_filters('smilies_src', ASSET_PATH . "/assets/img/smilies/{$s}.png", "{$s}.png", site_url())
+        . " alt='{$s}'></a>";
 }, [
     'razz',
     'evil',