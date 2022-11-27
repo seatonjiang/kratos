336ae9df2e9366c07e29fde441ef5983c8972ac2
Seaton Jiang <hi@seatonjiang.com>
2022-11-27 20:44:22 +0800

    feat: add page template (#500)

---
+++ page-full.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * Template Name: 单页样式
+ * @author Seaton Jiang <hi@seatonjiang.com>
+ * @license GPL-3.0 License
+ * @version 2022.11.27
+*/
+
+get_header(); ?>
+<div class="k-main <?php echo kratos_option('top_img_switch', true) ? 'banner' : 'color' ?>" style="background:<?php echo kratos_option('g_background', '#f5f5f5'); ?>">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 details">
+                <?php if (have_posts()) : the_post();
+                    update_post_caches($posts); ?>
+                    <div class="article py-4">
+                        <div class="header text-center">
+                            <h1 class="title m-0"><?php the_title(); ?></h1>
+                        </div>
+                        <div class="content">
+                            <?php
+                            the_content();
+                            wp_link_pages(
+                                array(
+                                    'before' => '<div class="paginations text-center">',
+                                    'after' => '',
+                                    'next_or_number' => 'next',
+                                    'previouspagelink' => __('<span>上一页</span>', 'kratos'),
+                                    'nextpagelink' => ''
+                                )
+                            );
+                            wp_link_pages(
+                                array(
+                                    'before' => '',
+                                    'after' => '',
+                                    'next_or_number' => 'number',
+                                    'link_before' => '<span>',
+                                    'link_after' => '</span>'
+                                )
+                            );
+                            wp_link_pages(
+                                array(
+                                    'before' => '',
+                                    'after' => '</div>',
+                                    'next_or_number' => 'next',
+                                    'previouspagelink' => '',
+                                    'nextpagelink' => __('<span>下一页</span>', 'kratos')
+                                )
+                            ); ?>
+                        </div>
+                    </div>
+                <?php endif; ?>
+                <?php comments_template(); ?>
+            </div>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
