105aa009dd15f350f47f1e70b472e8125d0e3857
Seaton Jiang <hi@seatonjiang.com>
2023-06-06 19:19:03 +0800

    feat: upgrade to version 4.2.2

--- assets/js/kratos.js
+++ assets/js/kratos.js
@@ -5,7 +5,7 @@
 (function ($) {
   "use strict";
 
-  var KRATOS_VERSION = "4.2.1";
+  var KRATOS_VERSION = "4.2.2";
 
   var navbarConfig = function () {
     $("#navbutton").on("click", function () {
